246cf470777525f286c8c752b43b2e07134e2579
Abdelrahman Rabah <68066903+r2ba7@users.noreply.github.com>
2022-07-03 22:11:00 +0200

    Create searchInsert.py

---
+++ searchInsert/searchInsert.py
@@ -0,0 +1,8 @@
+class Solution:
+    def searchInsert(self, nums: list, target: int) -> int:
+        if target in nums:
+            return nums.index(target)
+        else:
+            nums.append(target)
+            nums.sort()
+            return nums.index(target)
