087c0c967de17db700e552f10d06fc718c8aabdc
Abdelrahman Rabah <68066903+r2ba7@users.noreply.github.com>
2022-08-25 13:22:38 +0200

    Create lengthOfLastWord.py

---
+++ lengthOfLastWord/lengthOfLastWord.py
@@ -0,0 +1,11 @@
+class Solution:
+    def lengthOfLastWord(self, s: str) -> int:
+        s = s.strip()
+        chars = 0
+        for c in reversed(s):
+            if c == ' ':
+                return chars
+            chars+=1
+        return chars
+            
+        
