8cc4c0129507184b6a580ced23a84a09c1a60fb2
Abdelrahman Rabah <68066903+r2ba7@users.noreply.github.com>
2022-08-27 12:27:33 +0200

    Create canConstruct.py

---
+++ canConstruct/canConstruct.py
@@ -0,0 +1,9 @@
+from collections import Counter
+class Solution:
+
+    def canConstruct(self, ransomNote: str, magazine: str) -> bool:
+        magazine = Counter(magazine)
+        ransomNote = Counter(ransomNote)
+        return all(magazine[x] >= ransomNote[x] for x in ransomNote)
+        #return not Counter(ransomNote) - Counter(magazine)
+        
